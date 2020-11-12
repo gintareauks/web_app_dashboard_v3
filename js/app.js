e6b33d3fe2317866ed13dc49d5462561b98ed6e9
Gintare Auks <gintareauks@gmail.com>
2020-11-12 13:13:15 +0000

    JS small update

--- js/app.js
+++ js/app.js
@@ -1,41 +1,39 @@
-const alertBanner = document.getElementById('alert');
-const trafficCanvas = document.getElementById('traffic-chart');
-const dailyCanvas = document.getElementById('daily-chart');
-const mobileCanvas = document.getElementById('mobile-chart');
-const user = document.getElementById('userField');
-const message = document.getElementById('messageField');
-const send = document.getElementById('send');
+// ALERT BANNER
 
-//create the html for the banner 
+const alertBanner = document.getElementById("alert");
+
+// create the html for the banner 
 alertBanner.innerHTML = 
-`
-<div class="alert-banner">
-    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-    to complete</p>
-    <p class="alert-banner-close">x</p>
-</div>
-`;
-
+    `
+    <div class="alert-banner">
+        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+        <p class="alert-banner-close">x</p>
+    </div>
+    `
 alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
-        alert.style.display = "none"
+        alert.style.display = "none";
     }
-})
+});
+
+// CHART WIDGETS 
+
+// line graph
+const trafficCanvas = document.getElementById("traffic-chart");
 
 let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", 
 "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
-        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
-        2500],
-        backgroundColor: 'rgba(116, 119, 191, .3)',
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, .3',
         borderWidth: 1,
     }]
 };
 
 let trafficOptions = {
-    aspectRatio: 2.5,
+    aspectRatio: 2.5, 
     animation: {
         duration: 0
     },
@@ -45,20 +43,23 @@ let trafficOptions = {
                 beginAtZero: true
             }
         }]
-    }, 
+    },
     legend : {
         display: false
     }
 };
 
 let trafficChart = new Chart(trafficCanvas, {
-    type: 'line', 
-    data: trafficData, 
+    type: 'line',
+    data: trafficData,
     options: trafficOptions
 });
 
+// bar graph 
+const dailyCanvas = document.getElementById("daily-chart");
+// data for daily traffic bar chart
 const dailyData = {
-    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    labels: ["S", "M", "T", "W", "T", "F", "S"], 
     datasets: [{
         label: '# of Hits',
         data: [75, 115, 175, 125, 225, 200, 100],
@@ -69,7 +70,7 @@ const dailyData = {
 
 const dailyOptions = {
     scales: {
-        yAxes: [{
+        yAxes : [{
             ticks: {
                 beginAtZero: true
             }
@@ -78,27 +79,30 @@ const dailyOptions = {
     legend : {
         display: false
     }
-};
+}
 
 let dailyChart = new Chart(dailyCanvas, {
-    type: 'bar',
+    type: 'bar', 
     data: dailyData,
     options: dailyOptions
 });
 
+// doughnut chart
+const mobileCanvas = document.getElementById("mobile-chart");
+
 const mobileData = {
-    labels: ['Desktop', 'Tablet', 'Phones'], 
+    labels: ["Desktop", "Tablet", "Phones"], 
     datasets: [{
         label: '# of Users',
-        data: [2000, 550, 500], 
-        borderWidth: 0, 
+        data: [2000, 550, 500],
+        borderWidth: 0,
         backgroundColor: [
             '#7477BF',
-            '#78CF82',
+            '#78CD82',
             '#51B6C8'
         ]
     }]
-}
+};
 
 const mobileOptions = {
     legend: {
@@ -108,19 +112,24 @@ const mobileOptions = {
             fontStyle: 'bold'
         }
     }
-}
+};
 
 let mobileChart = new Chart(mobileCanvas, {
-    type: 'doughnut',
+    type: 'doughnut', 
     data: mobileData,
     options: mobileOptions
 });
+
+// MESSAGING SECTION
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
 
 send.addEventListener('click', () => {
 
     // ensure user and message fields are filled out
     if (user.value === "" && message.value === "") {
-        alert ("Please fill out user and message fields before sending");
+        alert("Please fill out user and message fields before sending");
     } else if (user.value === "") {
         alert("Please fill out user field before sending");
     } else if (message.value === "") {
@@ -130,3 +139,4 @@ send.addEventListener('click', () => {
     }
 });
 
+// 
